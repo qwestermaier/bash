e47c3f237f29f21cd08648a80ae4f93e612720da
Quirin Westermaier <38330880+qwestermaier@users.noreply.github.com>
2019-08-22 14:50:58 +0200

    Create kde5_gkrellm_set_wallpaper_feh.sh
    script to set current Wallpaper to feh background to enable invisible-themes in KDE5
    
    needs: feh

---
+++ kde5_gkrellm_set_wallpaper_feh.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#
+# Author: Quirin Westermaier
+# email: westermaier.quirin@gmail.com
+# github.com: qwestermaier
+#
+CURRENT_WALLPAPER_PLUGIN="`grep -m 1 'wallpaperplugin' ~/.config/plasma-org.kde.plasma.desktop-appletsrc | tail --bytes=+17`"
+echo "Currently set Wallpaper-Plugin: $CURRENT_WALLPAPER_PLUGIN"
+
+NEW_SEARCH_STRING="'\[$CURRENT_WALLPAPER_PLUGIN\]'"
+#echo "Now searching for $NEW_SEARCH_STRING"
+
+NEW_SEARCH_COMMAND_BLOCK="grep -m 1 -A 4 $NEW_SEARCH_STRING ~/.config/plasma-org.kde.plasma.desktop-appletsrc"
+#echo "Found: $NEW_SEARCH_COMMAND_BLOCK"
+
+CURRENT_WALLPAPER_IMAGE_BLOCK="`eval $NEW_SEARCH_COMMAND_BLOCK`"
+#echo "Configuration of currently set Wallpaper-Plugin: $CURRENT_WALLPAPER_IMAGE_BLOCK"
+
+CURRENT_WALLPAPER_IMAGE="`echo $CURRENT_WALLPAPER_IMAGE_BLOCK | tr ' ' '\n' | grep 'Image=' | tail --bytes=+14`"
+echo "Found Wallpaper Image: $CURRENT_WALLPAPER_IMAGE"
+
+echo "Executing feh: feh --bg-fill $CURRENT_WALLPAPER_IMAGE"
+feh --bg-fill "$CURRENT_WALLPAPER_IMAGE"
